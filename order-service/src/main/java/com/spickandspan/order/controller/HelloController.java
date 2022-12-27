bbcef2f9598a077fc957c3e8868a2dea09a2f3ad
ÖMER MUTLU <omermutlu29@gmail.com>
2022-12-27 20:01:20 +0300

    Keycloak finished

--- order-service/src/main/java/com/spickandspan/order/controller/HelloController.java
+++ order-service/src/main/java/com/spickandspan/order/controller/HelloController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/order")
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello from order";
+    public String hello(Principal principal) {
+
+        return principal.getName()+"Hello from order";
     }
 }
